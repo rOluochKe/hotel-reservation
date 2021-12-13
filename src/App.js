beb2035f937ef9e10f51c8d923e72de88a8815f3
roluochKe <rasholuoch@gmail.com>
2021-12-13 10:40:05 +0300

    routing and error page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
+import Error from './pages/Error'
+
+import Navbar from './components/Navbar'
 
 import './App.css'
 
 const App = () => {
   return (
-    <div className='app'>
-      <h1>app</h1>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/rooms/' component={Rooms} />
+        <Route exact path='/rooms/:slug' component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
   )
 }
 
