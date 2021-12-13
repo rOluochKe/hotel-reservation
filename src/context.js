dff5d6503486f3d4f5e48d04f8fe007412a8895e
roluochKe <rasholuoch@gmail.com>
2021-12-13 14:55:06 +0300

    filters in the rooms page

--- src/context.js
+++ src/context.js
@@ -72,6 +72,42 @@ export default class RoomProvider extends Component {
     )
   }
 
+  filterRooms = () => {
+    let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets } =
+      this.state
+
+    let tempRooms = [...rooms]
+    // transform values
+    // get capacity
+    capacity = parseInt(capacity)
+    price = parseInt(price)
+    // filter by type
+    if (type !== 'all') {
+      tempRooms = tempRooms.filter((room) => room.type === type)
+    }
+    // filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter((room) => room.capacity >= capacity)
+    }
+    // filter by price
+    tempRooms = tempRooms.filter((room) => room.price <= price)
+    //filter by size
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    )
+    //filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true)
+    }
+    //filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true)
+    }
+    this.setState({
+      sortedRooms: tempRooms,
+    })
+  }
+
   render() {
     return (
       <RoomContext.Provider
@@ -88,5 +124,15 @@ export default class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer
+
+export function withRoomConsumer(Component) {
+  return function ConsumerWrapper(props) {
+    return (
+      <RoomConsumer>
+        {(value) => <Component {...props} context={value} />}
+      </RoomConsumer>
+    )
+  }
+}
 
 export { RoomProvider, RoomConsumer, RoomContext }
