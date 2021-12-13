d79ab3b2e973e7e8af9ab589dc2f2f065a531b62
roluochKe <rasholuoch@gmail.com>
2021-12-13 16:20:06 +0300

    adding contentful

--- src/context.js
+++ src/context.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import items from './data'
+// import Client from './Contentful'
 
 const RoomContext = React.createContext()
 
@@ -21,6 +22,32 @@ export default class RoomProvider extends Component {
     breakfast: false,
     pets: false,
   }
+
+  // getData = async () => {
+  //   try {
+  //     let response = await Client.getEntries({
+  //       content_type: "beachResortRoom"
+  //     });
+  //     let rooms = this.formatData(response.items);
+
+  //     let featuredRooms = rooms.filter(room => room.featured === true);
+  //     //
+  //     let maxPrice = Math.max(...rooms.map(item => item.price));
+  //     let maxSize = Math.max(...rooms.map(item => item.size));
+  //     this.setState({
+  //       rooms,
+  //       featuredRooms,
+  //       sortedRooms: rooms,
+  //       loading: false,
+  //       //
+  //       price: maxPrice,
+  //       maxPrice,
+  //       maxSize
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   componentDidMount() {
     // this.getData();
