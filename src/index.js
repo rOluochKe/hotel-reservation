ed196292c8cae08618b16e6cc47fc867e4ea5607
roluochKe <rasholuoch@gmail.com>
2021-12-13 13:11:19 +0300

    fetch room

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { RoomProvider } from './context'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <RoomProvider>
+      <Router>
+        <App />
+      </Router>
+    </RoomProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
